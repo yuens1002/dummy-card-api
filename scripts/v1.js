24521c88f883d9ee90b5ef52a571cc8d832e7308
sunny yuen <yuens1002@gmail.com>
2016-12-22 10:37:27 -0800

    init for v1

---
+++ scripts/v1.js
@@ -0,0 +1,42 @@
+let deck = {
+
+	cards: [],
+	Card: class {
+		constructor(title, text, author, picUrl) {
+			this.id = undefined;
+			this.title = title;
+			this.text = text;
+			this.author = author;
+			this.picUrl = picUrl;
+		}
+	},
+	saveCards: function(arrOfCards) {
+		arrOfCards.forEach((card) => this.cards.push(card));
+	},
+	showCards: function() {
+		console.log(this.cards);
+	},
+	addCard: function(title, text, author, picUrl) {
+		this.cards.unshift(new this.Card(title, text, author, picUrl));
+		this.showCards();
+	},
+	editCard: function(position, title, text, author) {
+		this.cards[position].title = title;
+		this.cards[position].text = text;
+		this.cards[position].author = author;
+		this.showCards();
+	},
+	deleteCard: function(position) {
+		this.cards.splice(position, 1);
+		this.showCards();
+	},
+	fetchCards: function() {
+	  const url = 'http://localhost:3000/cards';
+	  fetch(url)
+	  	.then((result) => result.json())
+	  		.then((data) => this.saveCards(data))
+	    .catch((err) => console.log(err));
+	}
+};
+
+deck.fetchCards();
