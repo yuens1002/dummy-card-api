56e2cca1a4e37541cf04def278b6284356dce87c
sunny yuen <yuens1002@gmail.com>
2016-12-22 23:21:38 -0800

    added pagination and page info

--- scripts/v2.js
+++ scripts/v2.js
@@ -1,6 +1,7 @@
 let deck = {
 
 	cards: [],
+	cardsPerPage: 3,
 	Card: class {
 		constructor(title, text, author) {
 			this.id = undefined;
@@ -13,29 +14,46 @@ let deck = {
 	saveCards: function(arrOfCards) {
 		arrOfCards.forEach((card) => this.cards.push(card));
 	},
-	showCards: function() {
-		console.log(this.cards);
+	getNumPages: function() {
+		return Math.ceil(this.cards.length / this.cardsPerPage);
+	},
+	showCards: function(page) {
+		for (var i = (page - 1) * this.cardsPerPage; i < (page * this.cardsPerPage); i++) {
+    		if (this.cards[i]) console.log(this.cards[i]);
+		}
+		console.log('Page ' + page + ' of ' + this.getNumPages());
+		if (page >= 1 && page < this.getNumPages()) console.log ('next');
+		if (page > 1) console.log ('prev');
 	},
 	addCard: function(title, text, author, picUrl) {
 		this.cards.unshift(new this.Card(title, text, author));
-		this.showCards();
+		this.showCards(1);
 	},
 	editCard: function(position, title, text, author) {
 		this.cards[position].title = title;
 		this.cards[position].text = text;
 		this.cards[position].author = author;
-		this.showCards();
+		this.showCards(1);
 	},
 	deleteCard: function(position) {
 		this.cards.splice(position, 1);
-		this.showCards();
+		this.showCards(1);
 	},
 	fetchCards: function() {
-	  const url = 'http://localhost:3000/cards';
-	  fetch(url)
+	  const BaseUrl = 'http://localhost:3000/cards';
+	  const threeCards = '?_page=1&_limit=99';
+	  let curl = BaseUrl + threeCards;
+
+// research server-side pagination using header Link
+//	  fetch(curl)
+//	  	.then((result) => (result.headers.get('Link')))
+//	    .catch((err) => console.log(err));
+	  fetch(curl)
 	  	.then((result) => result.json())
 	  		.then((data) => this.saveCards(data))
+	        .then(() => this.showCards(1))
 	    .catch((err) => console.log(err));
+
 	}
 };
 
