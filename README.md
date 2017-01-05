0a074c4bd975970d5c8e5b13f2a36bc7a8054a93
sunny yuen <yuens1002@gmail.com>
2017-01-05 08:02:03 -0800

    completed specs for v8

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-next 1. should render fonts accordingly to various page widths
-done 2. should check for errors before saving user edits
-done 2a. should have one form check function for both new and edit modes
-50% 3. should do a general sweep of code clean up
-4. may look at a different way of loading images that do not cause slight pauses during animation with start and end animation lisetners
+1. should render fonts accordingly to various page widths
+2. should check for errors before saving user edits
+2a. should have one form check function for both new and edit modes
+3. should do a general sweep of code clean up
--- animation tests/object-literal.js
+++ animation tests/object-literal.js
@@ -43,3 +43,46 @@ formCheck function(formFields) {
 	}
 
 }
+
+function formCheck(formFields) {
+    
+    let newCard = {
+		title: function() {
+			console.log('NewCard title is called')	
+		},
+		text: function() {
+			console.log('NewCard text is called')
+		},
+		author: function() {
+			console.log('NewCard author is called')
+		},
+        processForm: function () {
+            for (let key in formFields) {
+              !(!!formFields[key]) && this[key]();
+            }
+        }
+	};
+	let editCard = {
+		title: function() {
+			console.log('Edited title is called')	
+		},
+		text: function() {
+			console.log('Edited text is called')	
+		},
+        processForm: function () {
+            for (let key in formFields) {
+              !(!!formFields[key]) && this[key]();
+            }
+        }
+	};
+  
+    function processForm (formType) {
+        for (let key in formFields) {
+                !!!formFields[key] && formType[key]();
+        }
+    }
+  
+    processForm(newCard);
+}
+
+formCheck({title: '', text: 'hello', author: 'tom'});
